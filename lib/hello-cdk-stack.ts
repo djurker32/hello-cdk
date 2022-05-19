a02365f10b07de34fb4e5690883e2a9fe9e91b8c
Yurii Vydai <yurii.vydai@intellias.com>
2022-05-19 09:25:53 +0000

    lambda

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,11 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
-import { aws_s3 as s3 } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // This creates the template that makes my bucket
-    const bucket = new s3.Bucket(this, 'HelloWorldCDKFuntime');
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
+    });
   }
 }
